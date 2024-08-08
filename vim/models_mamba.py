0c6877767c904864ec3c58f058f080d0de05ce7c
YounSeokHyun <hisn16@naver.com>
2024-08-08 14:41:07 +0900

    bug fix

--- vim/models_mamba.py
+++ vim/models_mamba.py
@@ -673,7 +673,7 @@ def model_a(pretrained=False, **kwargs):
 def cifar_model_a(pretrained=False, **kwargs):
     window_sizes = [0]*4 + [7]*4 + [2]*4 + [1]*4
     model = VisionMamba(
-        patch_size=5, stride=2, img_size=32, num_classes=100, embed_dim=192, depth=16, rms_norm=True, residual_in_fp32=True, fused_add_norm=True, final_pool_type='mean', if_abs_pos_embed=True, if_rope=False, if_rope_residual=False, bimamba_type="v2", if_cls_token=True, if_divide_out=True, use_middle_cls_token=True, window_sizes=window_sizes, **kwargs)
+        patch_size=5, stride=2, img_size=32, embed_dim=192, depth=16, rms_norm=True, residual_in_fp32=True, fused_add_norm=True, final_pool_type='mean', if_abs_pos_embed=True, if_rope=False, if_rope_residual=False, bimamba_type="v2", if_cls_token=True, if_divide_out=True, use_middle_cls_token=True, window_sizes=window_sizes, **kwargs)
     model.default_cfg = _cfg()
     if pretrained:
         checkpoint = torch.hub.load_state_dict_from_url(
@@ -687,7 +687,7 @@ def cifar_model_a(pretrained=False, **kwargs):
 def cifar_model_b(pretrained=False, **kwargs):
     window_sizes = [0]*4 + [1]*4 + [2]*4 + [7]*4
     model = VisionMamba(
-        patch_size=5, stride=2, img_size=32, num_classes=100, embed_dim=192, depth=16, rms_norm=True, residual_in_fp32=True, fused_add_norm=True, final_pool_type='mean', if_abs_pos_embed=True, if_rope=False, if_rope_residual=False, bimamba_type="v2", if_cls_token=True, if_divide_out=True, use_middle_cls_token=True, window_sizes=window_sizes, **kwargs)
+        patch_size=5, stride=2, img_size=32, embed_dim=192, depth=16, rms_norm=True, residual_in_fp32=True, fused_add_norm=True, final_pool_type='mean', if_abs_pos_embed=True, if_rope=False, if_rope_residual=False, bimamba_type="v2", if_cls_token=True, if_divide_out=True, use_middle_cls_token=True, window_sizes=window_sizes, **kwargs)
     model.default_cfg = _cfg()
     if pretrained:
         checkpoint = torch.hub.load_state_dict_from_url(
