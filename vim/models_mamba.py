8c7520cf341ba6d56199a5d230f6d4b16c94403e
SH-Youn-01 <hisn16@naver.com>
2024-08-10 01:53:18 +0900

    bug fix

--- vim/models_mamba.py
+++ vim/models_mamba.py
@@ -651,7 +651,7 @@ def vim_base_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_middle_cls
 @register_model
 def cifar_baseline(pretrained=False, **kwargs):
     model = VisionMamba(
-        patch_size=8, stride=4, embed_dim=192, depth=24, rms_norm=True, residual_in_fp32=True, fused_add_norm=True, final_pool_type='mean', if_abs_pos_embed=True, if_rope=False, if_rope_residual=False, bimamba_type="v2", if_cls_token=True, if_divide_out=True, use_middle_cls_token=True, **kwargs)
+        patch_size=5, stride=2, embed_dim=192, depth=24, rms_norm=True, residual_in_fp32=True, fused_add_norm=True, final_pool_type='mean', if_abs_pos_embed=True, if_rope=False, if_rope_residual=False, bimamba_type="v2", if_cls_token=True, if_divide_out=True, use_middle_cls_token=True, **kwargs)
     model.default_cfg = _cfg()
     if pretrained:
         checkpoint = torch.hub.load_state_dict_from_url(
